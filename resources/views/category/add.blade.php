1a38f31caf844ee497f6779e932b97f0f81f967f
Thai Tran <thai.ttran07@gmail.com>
2021-10-21 08:23:58 +0700

    Use recursion to show multi-level categories

--- resources/views/category/add.blade.php
+++ resources/views/category/add.blade.php
@@ -18,21 +18,18 @@
         <div class="col-md-12">
           <a href="{{ route('categories.create') }}" class="btn btn-primary float-right m-2">Add</a>
         </div>
-        <div class="col-md-12">
+        <div class="col-md-6">
           <form>
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <label for="exampleInputEmail1" class="form-label">Categories's name</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
               <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
+            <select class="form-control" aria-label="Default select example">
+              <option selected>Choose parent category</option>
+              {!! $htmlOption !!}
+            </select>
+            <br>
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
