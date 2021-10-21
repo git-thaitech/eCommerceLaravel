bc9b7c92873c5861aab42ef753a5b8b1cc61b280
Thai Tran <thai.ttran07@gmail.com>
2021-10-21 11:58:03 +0700

    Add a function in DatabaseSeeders to create multi-level Categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,18 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        foreach (range(1, 5) as $index) {
+        $this->multiLevelCategories(3, 2, 3);
+    }
+
+    public function multiLevelCategories($num1 = 0, $num2 = 0, $num3 = 0)
+    {
+        foreach (range(1, $num1) as $index1) {
             DB::table('categories')->insert([
-                'name' => 'Category ' . $index,
+                'name' => 'Category ' . $index1,
                 'parent_id' => '0',
                 'slug' => ''
             ]);
-            foreach (range(1, 3) as $index2) {
+            foreach (range(1, $num2) as $index2) {
                 DB::table('categories')->insert([
-                    'name' => 'Category ' . $index . '.' . $index2,
-                    'parent_id' => $index + ($index - 1) * 3,
+                    'name' => 'Category ' . $index1 . '.' . $index2,
+                    'parent_id' => ($num2 * $num3 + $num2 + 1) * ($index1 - 1) + 1,
                     'slug' => ''
                 ]);
+                foreach (range(1, $num3) as $index3) {
+
+                    DB::table('categories')->insert([
+                        'name' => 'Category ' . $index1 . '.' . $index2 . '.' . $index3,
+                        'parent_id' => ($num2 * $num3 + $num2 + 1) * ($index1 - 1) + ($index2 - 1) * $num3 + 1 + $index2,
+                        'slug' => ''
+                    ]);
+                }
             }
         }
     }
